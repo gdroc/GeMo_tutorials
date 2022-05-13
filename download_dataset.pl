8e6d140d0425f49e5953b71619b1102736c53583
gdroc <gaetan.droc@cirad.fr>
2022-05-13 10:58:24 +0200

    Minor debug

--- download_dataset.pl
+++ download_dataset.pl
@@ -8,7 +8,7 @@ my $citrus_dataset = "https://zenodo.org/record/6542870/files/Ahmed_et_al_2019.z
 my $banana_dataset = "https://zenodo.org/record/6542870/files/Baurens_et_al_2019.zip";
 
 unless (-e "data/Baurens_et_al_2019.vcf") {
-    system("mkdir test1");
-    system("curl $banana_dataset -o data/Baurens_et_al_2019.zip;cd data; unzip Baurens_et_al_2019.zip");
-    system("curl $citrus_dataset -o data/Ahmed_et_al_2019.zip;cd data;unzip Ahmed_et_al_2019.zip");
+    system("mkdir data");
+    system("curl --silent $banana_dataset -o data/Baurens_et_al_2019.zip;cd data; unzip Baurens_et_al_2019.zip");
+    system("curl --silent $citrus_dataset -o data/Ahmed_et_al_2019.zip;cd data;unzip Ahmed_et_al_2019.zip");
 }
